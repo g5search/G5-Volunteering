44fffb143488c9ca8e8e1b6eef8f69e380375c50
Bryce Sampson <Bryce.Sampson@bend.getg5.com>
2020-01-24 11:05:00 -0800

    add dotenv to hide sensitive info

--- src/plugins/firebaseConfig.js
+++ src/plugins/firebaseConfig.js
@@ -1,14 +1,17 @@
 import firebase from 'firebase'
 import 'firebase/firestore'
+require('dotenv').config()
+
+
 
 const config = {
-    apiKey: "AIzaSyCA98aJtdW_R4j4wmyf2spxw1XlHiSwMvk",
-    authDomain: "vhours-3c992.firebaseapp.com",
-    databaseURL: "https://vhours-3c992.firebaseio.com",
-    projectId: "vhours-3c992",
-    storageBucket: "vhours-3c992.appspot.com",
-    messagingSenderId: "937326836465",
-    appId: "1:937326836465:web:bfa387a0cb16c956578750"
+    apiKey: process.env.VUE_APP_API_KEY,
+    authDomain: process.env.VUE_APP_AUTH_DOMAIN,
+    databaseURL: process.env.VUE_APP_DATABASE_URL,
+    projectId: process.env.VUE_APP_PROJECT_ID,
+    storageBucket: process.env.VUE_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.VUE_APP_MESSAGING_ID,
+    appId: process.env.VUE_APP_APP_ID
 }
 
 firebase.initializeApp(config)
